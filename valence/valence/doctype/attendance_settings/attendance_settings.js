ae1b89add4b4c409d6f31e3c1475c37e1d09cb6c
Finbyz <info@finbyz.tech>
2025-04-25 16:42:40 +0530

    by - Attendance Seetings Doctype

---
+++ valence/valence/doctype/attendance_settings/attendance_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, finbyz tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Attendance Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });

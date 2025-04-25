c72a6709fdec7e105e843e09c2fc5e93cbc3411f
Finbyz <info@finbyz.tech>
2025-04-25 16:45:21 +0530

    by - attendace override function

---
+++ valence/valence/override/attendance.py
@@ -0,0 +1,19 @@
+import frappe
+from frappe import _
+from hrms.hr.doctype.attendance.attendance import Attendance as _Attendance, validate_active_employee
+
+class Attendance(_Attendance):
+	def before_insert(self):
+		if self.half_day_status == "":
+			self.half_day_status = None
+
+	def validate(self):
+		from erpnext.controllers.status_updater import validate_status
+
+		validate_status(self.status, ["Present", "Absent", "On Leave", "Half Day", "Work From Home","In Mispunch","Out Mispunch","No punch","Present With Short Leave","Holiday","Weekly off"])
+		validate_active_employee(self.employee)
+		self.validate_attendance_date()
+		self.validate_duplicate_record()
+		self.validate_overlapping_shift_attendance()
+		self.validate_employee_status()
+		self.check_leave_record()

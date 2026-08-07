15a49641b9f10cfefddb95897a40e4a962a0e78a
Jaskirat Singh <js4324093@gmail.com>
2026-08-07 11:40:55 +0000

    Add Quarterly Working Days DocType
    Manual-entry fallback for #9 (Automatic Leave Balance Update).
    HR fills this in per employee/quarter when the system can't
    auto-calculate working days from Attendance. Fields: employee,
    company, quarter_start_date, quarter_end_date, working_days,
    is_manual_entry.

---
+++ valence/valence/doctype/quarterly_working_days/quarterly_working_days.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, finbyz tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Quarterly Working Days", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ valence/valence/doctype/quarterly_working_days/test_quarterly_working_days.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, finbyz tech and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQuarterlyWorkingDays(FrappeTestCase):
+	pass
